aae5a2a4c3613c207f38baac2ee7b3094ac2b806
Exallotriote <lordofchaos892@gmail.com>
2023-03-12 21:19:54 +0800

    minified download.js code
    # removed the following functions:
    - getText
    - getListText
    - getSrc
    
    # Changed
    - minified the download() function a bit

--- download.js
+++ download.js
@@ -3,38 +3,14 @@ function download(filename, text) {
   var element = document.createElement('a');
   element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
   element.setAttribute('download', filename);
-
   element.style.display = 'none';
   document.body.appendChild(element);
-
   element.click();
-
-  document.body.removeChild(element);
-}
-
-
+  document.body.removeChild(element);}
 //--- Functions
 function log(vari){console.log(vari)}
-
-function getText(selector) {
-	return document.querySelector(selector).innerText
-}
-
-function getListText(selector, num){
-	return document.querySelector(selector).childNodes[num].innerText
-}
-
-function getSrc (selector){
-	return document.querySelector(selector).getAttribute('src')
-}
-
-function getLink (selector){
-	return document.querySelector(selector).href
-}
-
-function getHtml(selector) {
-	return document.querySelector(selector).innerHTML
-}
+function getLink (selector){return document.querySelector(selector).href}
+function getHtml(selector) {return document.querySelector(selector).innerHTML}
 
 //--- Code
 
